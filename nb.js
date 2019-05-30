a2851b241277a0681c22ace6b3c749d65501bd5d
Courtney Fay <fay.courtney@gmail.com>
2019-05-30 08:07:18 -1000

    fixing bad variable name

--- nb.js
+++ nb.js
@@ -101,10 +101,10 @@ setChordCountsInLabels();
 setProbabilityOfChordsInLabels();
 
 function classify(chords) {
-  var ttal = labelProbabilities;
-  console.log(ttal);
+  var total = labelProbabilities;
+  console.log(total);
   var classified = {};
-  Object.keys(ttal).forEach(function(obj) {
+  Object.keys(total).forEach(function(obj) {
     var first = labelProbabilities[obj] + 1.01;
     chords.forEach(function(chord) {
       var probabilityOfChordsInLabel = probabilityOfChordsInLabels[obj][chord];
