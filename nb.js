5b6f43e47b629ad32bd58f3302a9ced1659306c8
Courtney Fay <fay.courtney@gmail.com>
2019-05-31 15:00:34 -0700

    moving difficulties into their own function

--- nb.js
+++ nb.js
@@ -7,9 +7,12 @@ function welcomeMessage() {
   return `Welcome to ${fileName()}!`;
 }
 
-var easy = "easy";
-var medium = "medium";
-var hard = "hard";
+function setDifficulties() {
+  easy = "easy";
+  medium = "medium";
+  hard = "hard";
+}
+setDifficulties();
 
 function setSongs() {
   imagine = ["c", "cmaj7", "f", "am", "dm", "g", "e7"];
